c33eba4278a3d9b6f2ef78b5f9842d0c1f1e4e54
khushi08928 <khushichawla105@gmail.com>
2026-01-09 13:39:17 +0530

    initial setup of project

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import {createRouter, RouterProvider} from "@tanstack/react-router";
+import {routeTree} from "./routes"
+
+const router = createRouter({
+  routeTree,
+})
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+function App() {
+  return <RouterProvider router={router} />
+}
+    
+export default App
