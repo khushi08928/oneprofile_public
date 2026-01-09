a5367a5caf9780c6e86f1e4459defc52eac67c9c
khushi08928 <khushichawla105@gmail.com>
2026-01-09 14:03:22 +0530

    add proxy for backend request calling

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,16 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api":{
+        target:"http://localhost:4000",
+        changeOrigin:true,
+        rewrite: (path) => path.replace(/^\/api/, "/api/v1"),
+        secure:true,
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
