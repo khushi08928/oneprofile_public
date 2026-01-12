84a2893883576df715c662b61260ae509c559e1c
khushi08928 <khushichawla105@gmail.com>
2026-01-12 16:58:05 +0530

    add new dependencies and update Vite configuration for environment support

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,28 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api":{
-        target:"http://localhost:4000",
-        changeOrigin:true,
-        rewrite: (path) => path.replace(/^\/api/, "/api/v1"),
-        secure:true,
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_BACKEND_URL || "http://localhost:4000",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, "/api/v1"),
+          secure: true,
+        },
       },
     },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  }
 })
