9f7f1478177e513123c4817bcbc09c908c02f04d
khushi08928 <khushichawla105@gmail.com>
2026-01-13 17:04:18 +0530

    refactor: format import statement for axios and isAxiosError

--- src/components/ui/SignInForm.tsx
+++ src/components/ui/SignInForm.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import axios, {isAxiosError} from "@/lib/axios";
+import axios, { isAxiosError } from "@/lib/axios";
 import { loginSchema } from "@/schemas/LoginSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Link, useNavigate } from "@tanstack/react-router";
